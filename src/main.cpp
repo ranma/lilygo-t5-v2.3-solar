d58398223714a76410516e8f9f53563fb84785e3
Tobias Diedrich <ranma+git@tdiedrich.de>
2024-10-13 15:44:12 +0200

    Optimized reconnect.
    rst:0x5 (DEEPSLEEP_RESET),boot:0x17 (SPI_FAST_FLASH_BOOT)
    configsip: 0, SPIWP:0xee
    clk_drv:0x00,q_drv:0x00,d_drv:0x00,cs0_drv:0x00,hd_drv:0x00,wp_drv:0x00
    mode:DIO, clock div:1
    load:0x3fff0030,len:1344
    load:0x40078000,len:13964
    load:0x40080400,len:3600
    entry 0x400805f0
    Hello world (wake=4) bat=4.21V t=68.9!
    Initialize FS... done
    [22] WiFi ready
    Re-using IP a808a8c0 (lease=43200 renew=21480)
    [35] WiFi STA start
    Reconnecting to ch=1 bss=04f02123272a
    Default txpower=78
    [39] skipping full display init!
    [227] WiFi STA connected (ch=1 bss=04f02123272a)
    [228] WiFi STA got ip: 192.168.8.168/255.255.255.0 gw 192.168.8.254 rssi=-48
    [456] setup done
    [519] Connected to MQTT (0)
    [1268] received topic solar/battery/current: 0.30
    [1268] received topic solar/battery/stateOfCharge: 69.00
    [1268] received topic solar/ac/power: 257.1
    [1270] update display
    [1272] WiFi STA disconnected: 8
    [1275] WiFi STA stop
    [1593] going to sleep for 28407ms
    [1827] esp_deep_sleep_start()

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,31 @@
 #include <ESP32AnalogRead.h>
 #include <Fonts/FreeSans18pt7b.h>
 #include <espMqttClientAsync.h>
+#include <esp_wifi.h>
+#include <esp_netif.h>
+#include <lwip/netif.h>
+#include <lwip/dhcp.h>
+
+// Partial copy of components/esp_netif/lwip/esp_netif_lwip_internal.h
+struct esp_netif_obj {
+	uint8_t mac[NETIF_MAX_HWADDR_LEN];
+	void* ip_info;
+	void* ip_info_old;
+	struct netif *lwip_netif;
+	// don't care about remaining entries here.
+};
+
+// extract the DHCP lease time.
+static uint32_t netif_get_dhcp_lease(esp_netif_t *netif) {
+	if (netif != nullptr && netif->lwip_netif != nullptr) {
+		struct netif *lwip_netif = netif->lwip_netif;
+		struct dhcp *dhcp = netif_dhcp_data(lwip_netif);
+		if (dhcp != nullptr) {
+			return dhcp->t0_timeout;
+		}
+	}
+	return 0;
+}
 
 #define MAX_DISPLAY_BUFFER_SIZE 65536ul
 #define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8))
@@ -17,7 +42,20 @@ GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, MAX_HEIGHT(GxEPD2_DRIVER_CLASS)> displ
 SPIClass hspi(HSPI);
 espMqttClientAsync mqttClient;
 
+struct rtc_config {
+	uint8_t bssid[6];
+	uint8_t channel;
+	uint32_t ip;
+	uint32_t gateway;
+	uint32_t netmask;
+	uint32_t primaryDNS;
+	uint32_t secondaryDNS;
+	int dhcp_lease;
+	int dhcp_renew_remain;
+};
+
 static RTC_DATA_ATTR int survives_sleep = 0;
+static RTC_DATA_ATTR struct rtc_config rtc_config;
 
 static float batteryCurrent = NAN;
 static float batterySoC = NAN;
@@ -63,21 +101,49 @@ static void fs_init(bool full_init)
 
 static void wifi_event_cb(arduino_event_id_t event, arduino_event_info_t info)
 {
+    uint8_t *bssid;
     switch (event) {
-    case ARDUINO_EVENT_WIFI_STA_CONNECTED:
-        printf("[%ld] WiFi connected\n", millis());
+    case ARDUINO_EVENT_WIFI_READY: // 0
+        printf("[%ld] WiFi ready\n", millis());
         break;
-    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+    case ARDUINO_EVENT_WIFI_STA_START: // 2
+        printf("[%ld] WiFi STA start\n", millis());
+        break;
+    case ARDUINO_EVENT_WIFI_STA_STOP: // 3
+        printf("[%ld] WiFi STA stop\n", millis());
+        break;
+    case ARDUINO_EVENT_WIFI_STA_CONNECTED: // 4
+        bssid = info.wifi_sta_connected.bssid;
+        printf("[%ld] WiFi STA connected (ch=%d bss=%02x%02x%02x%02x%02x%02x)\n",
+		millis(), info.wifi_sta_connected.channel, bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5]);
+	rtc_config.channel = info.wifi_sta_connected.channel;
+	memcpy(rtc_config.bssid, info.wifi_sta_connected.bssid, sizeof(rtc_config.bssid));
+        break;
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED: // 5
         // Reason codes can be found here: https://github.com/espressif/esp-idf/blob/5454d37d496a8c58542eb450467471404c606501/components/esp_wifi/include/esp_wifi_types_generic.h#L79-L141
-        printf("WiFi disconnected: %d\n", info.wifi_sta_disconnected.reason);
-	goToSleep = true;
+        printf("[%ld] WiFi STA disconnected: %d\n", millis(), info.wifi_sta_disconnected.reason);
+        goToSleep = true;
         break;
-    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
-        printf("[%ld] WiFi got ip: %s\n", millis(), WiFi.localIP().toString().c_str());
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP: // 7
+        printf("[%ld] WiFi STA got ip: %s/%s gw %s rssi=%d\n", millis(),
+		 WiFi.localIP().toString().c_str(),
+		 WiFi.subnetMask().toString().c_str(),
+		 WiFi.gatewayIP().toString().c_str(),
+		 WiFi.RSSI());
+	rtc_config.ip = WiFi.localIP();
+	rtc_config.gateway = WiFi.gatewayIP();
+	rtc_config.netmask = WiFi.subnetMask();
+	rtc_config.primaryDNS = WiFi.dnsIP(0);
+	rtc_config.secondaryDNS = WiFi.dnsIP(1);
+	if (!rtc_config.dhcp_lease) {
+		rtc_config.dhcp_lease = netif_get_dhcp_lease(info.got_ip.esp_netif);
+		rtc_config.dhcp_renew_remain = rtc_config.dhcp_lease / 2;
+	}
+
         mqttClient.connect();
         break;
     default:
-        printf("Unhandled event: %d\n", event);
+        printf("[%ld] Unhandled event: %d (%s)\n", millis(), event, WiFi.eventName(event));
         break;
     }
 }
@@ -93,7 +159,24 @@ static void wifi_init()
 	WiFi.mode(WIFI_MODE_NULL);
 	WiFi.setHostname(hostname.c_str());
 	WiFi.mode(WIFI_MODE_STA);
-	WiFi.begin(wifi_ssid, wifi_password, WIFI_ALL_CHANNEL_SCAN);
+	if (rtc_config.channel) {
+		uint8_t *bssid = rtc_config.bssid;
+		if (rtc_config.dhcp_lease) {
+			printf("Re-using IP %08x (lease=%d renew=%d)\n", rtc_config.ip, rtc_config.dhcp_lease, rtc_config.dhcp_renew_remain);
+			WiFi.config(rtc_config.ip, rtc_config.gateway, rtc_config.netmask,
+				rtc_config.primaryDNS, rtc_config.secondaryDNS);
+			if (rtc_config.dhcp_renew_remain < 0) {
+				rtc_config.dhcp_lease = 0;
+			}
+		}
+		printf("Reconnecting to ch=%d bss=%02x%02x%02x%02x%02x%02x\n",
+			rtc_config.channel, bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5]);
+		WiFi.begin(wifi_ssid, wifi_password, rtc_config.channel, rtc_config.bssid);
+	} else {
+		WiFi.begin(wifi_ssid, wifi_password, WIFI_ALL_CHANNEL_SCAN);
+	}
+	printf("Default txpower=%d\n", WiFi.getTxPower());
+	WiFi.setTxPower(WIFI_POWER_11dBm);
 }
 
 constexpr char kMqttBatteryCurrent[] = "solar/battery/current";
@@ -139,6 +222,8 @@ void onMqttMessage(const espMqttClientTypes::MessageProperties& properties, cons
 	}
 
 	dataReady = true;
+	WiFi.disconnect(true);
+	WiFi.mode(WIFI_OFF);
 }
 
 static void mqtt_init()
@@ -175,9 +260,9 @@ void setup()
 	display.epd2.selectSPI(hspi, SPISettings(10000000, MSBFIRST, SPI_MODE0));
 
 	if (!full_init) {
-		printf("skipping full display init!\n");
+		printf("[%ld] skipping full display init!\n", millis());
 	}
-	display.init(115200, full_init);
+	display.init(0/*diag disabled*/, full_init);
 	display.setRotation(1);
 	display.setFullWindow();
 	if (full_init) {
@@ -192,19 +277,16 @@ void setup()
 		// trigger power-on.
 		display.refresh(true);
 	}
+	printf("[%ld] setup done\n", millis());
 }
 
 void loop()
 {
-	static int i = 0;
-	if (!(i++ % 10)) {
-		printf("Loop%d host=%s ssid=%s\n", i++, hostname.c_str(), wifi_ssid.c_str());
-	}
-	delay(100);
 	if (dataReady) {
 		float p = (1-(4.1-intBatV)/(4.1-3.3))*100;
 		p = p < 100 ? p : 100;
 
+		printf("[%ld] update display\n", millis());
 		display.firstPage();
 		display.fillScreen(GxEPD_WHITE);
 		display.setTextColor(GxEPD_BLACK);
@@ -216,14 +298,27 @@ void loop()
 		display.display(/*partial*/ true);
 		goToSleep = true;
 	}
-	if (goToSleep || i >= 200) {
-		long sleepMillis = 30 * 1000 - millis();
+	if (goToSleep || millis() >= 20000) {
+		long sleepTime = 30;
+		rtc_config.dhcp_renew_remain -= sleepTime;
+
+		long sleepMillis = sleepTime * 1000 - millis();
 		printf("[%ld] going to sleep for %ldms\n", millis(), sleepMillis);
-		WiFi.disconnect(true);
-		WiFi.mode(WIFI_OFF);
 		display.hibernate();
 		esp_sleep_enable_timer_wakeup(sleepMillis * 1000);
+		// Make sure epaper CS# is kept high during sleep.
+		pinMode(16, INPUT_PULLUP);
 		printf("[%ld] esp_deep_sleep_start()\n", millis());
+		gpio_deep_sleep_hold_en();
+#if 0
+		// Make sure SDIO power domain (SPI flash) is disabled during sleep.
+		// Doesn't do anything here, as it's also directly connected to +3V3...
+		esp_sleep_pd_config(ESP_PD_DOMAIN_VDDSDIO, ESP_PD_OPTION_OFF)
+#endif
 		esp_deep_sleep_start();
+
+		// Deep sleep power draw measured ~394uA after adding a pull-up
+		// to the SPI flash CS# (3.6mA before).
 	}
+	delay(1);
 }
