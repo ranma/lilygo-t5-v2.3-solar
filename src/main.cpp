4986176b8c4afabb2ad10a2f6139a103c7cfe5df
Tobias Diedrich <ranma+git@tdiedrich.de>
2024-10-14 21:23:28 +0200

    Get image from custom tcp server
    Reduces time connected to WiFi to ~500ms in the good case:
    
    [101] skipping full display init!
    [283] WiFi STA connected
    [284] WiFi STA got ip
    [295] +C: 0x00000000 0x3ffbd920 0
    [299] +S: 0x00000000 0x3ffbd920 19
    [304] +R: 0x00000000 0x3ffbd920 0
    rx 1436 bytes
    bmp size: 3966 (pixbuf @62)
    [306] +R: 0x00000000 0x3ffbd920 0
    rx 1436 bytes
    [308] +R: 0x00000000 0x3ffbd920 0
    rx 1094 bytes
    [311] +E: 0x00000000 -13
    [518] setup done
    ulp_adc_data[0] = 2083 (2349/92d)
    ulp_adc_data[1] = 2084 (2351/92f)
    ulp_adc_data[2] = 2083 (2350/92e)
    ulp_adc_data[3] = 2083 (2350/92e)
    ulp_adc_data[4] = 2074 (2338/922)
    ulp_adc_data[5] = 2083 (2349/92d)
    ulp_adc_data[6] = 2079 (2345/929)
    ulp_adc_data[7] = 2083 (2349/92d)
    ulp_adc_data[56] = 2078 (2343/927)
    ulp_adc_data[57] = 2078 (2343/927)
    ulp_adc_data[58] = 2078 (2343/927)
    ulp_adc_data[59] = 2078 (2344/928)
    ulp_adc_data[60] = 2077 (2342/926)
    ulp_adc_data[61] = 2077 (2342/926)
    ulp_adc_data[62] = 2080 (2346/92a)
    ulp_adc_data[63] = 2078 (2343/927)
    [558] update display
    [562] WiFi STA disconnected: 8
    [563] Unhandled event: 3 (STA_STOP)
    [1211] esp_deep_sleep_start()
    [1211] going to sleep for 28789ms

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,27 @@
 #include <WiFi.h>
 #include <ESP32AnalogRead.h>
 #include <Fonts/FreeSans18pt7b.h>
-#include <espMqttClientAsync.h>
+#include <esp_adc_cal.h>
 #include <esp_wifi.h>
 #include <esp_netif.h>
+#include <esp_log.h>
+#include <esp32/ulp.h>
+#include <driver/rtc_io.h>
+#include <hal/sar_ctrl_ll.h>
+
+extern "C"{
+#include <lwip/pbuf.h>
+#include <lwip/opt.h>
+#include <lwip/tcp.h>
+#include <lwip/tcpip.h>
+#include <lwip/inet.h>
 #include <lwip/netif.h>
 #include <lwip/dhcp.h>
+}
+
+#if CONFIG_ESP32_ULP_COPROC_RESERVE_MEM != 512
+#error Unexpected reserved mem value for ULP
+#endif
 
 // Partial copy of components/esp_netif/lwip/esp_netif_lwip_internal.h
 struct esp_netif_obj {
@@ -40,7 +56,6 @@ static uint32_t netif_get_dhcp_lease(esp_netif_t *netif) {
 GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, MAX_HEIGHT(GxEPD2_DRIVER_CLASS)> display(GxEPD2_DRIVER_CLASS(/*CS=5*/ 5, /*DC=*/ 17, /*RST=*/ 16, /*BUSY=*/ 4));
 
 SPIClass hspi(HSPI);
-espMqttClientAsync mqttClient;
 
 struct rtc_config {
 	uint8_t bssid[6];
@@ -57,14 +72,12 @@ struct rtc_config {
 static RTC_DATA_ATTR int survives_sleep = 0;
 static RTC_DATA_ATTR struct rtc_config rtc_config;
 
-static float batteryCurrent = NAN;
-static float batterySoC = NAN;
-static float inverterPower = NAN;
 static float intBatV = NAN;
+static bool pixbufReady = false;
 static bool dataReady = false;
 static bool goToSleep = false;
 
-constexpr int batPin = 35;
+constexpr int batPin = 35; // ADC1 input 7
 
 
 extern const uint8_t hostname_start[] asm("_binary_cfg_hostname_start");
@@ -99,53 +112,146 @@ static void fs_init(bool full_init)
 	printf("failed\n");
 }
 
-static void wifi_event_cb(arduino_event_id_t event, arduino_event_info_t info)
-{
-    uint8_t *bssid;
-    switch (event) {
-    case ARDUINO_EVENT_WIFI_READY: // 0
-        printf("[%ld] WiFi ready\n", millis());
-        break;
-    case ARDUINO_EVENT_WIFI_STA_START: // 2
-        printf("[%ld] WiFi STA start\n", millis());
-        break;
-    case ARDUINO_EVENT_WIFI_STA_STOP: // 3
-        printf("[%ld] WiFi STA stop\n", millis());
-        break;
-    case ARDUINO_EVENT_WIFI_STA_CONNECTED: // 4
-        bssid = info.wifi_sta_connected.bssid;
-        printf("[%ld] WiFi STA connected (ch=%d bss=%02x%02x%02x%02x%02x%02x)\n",
-		millis(), info.wifi_sta_connected.channel, bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5]);
-	rtc_config.channel = info.wifi_sta_connected.channel;
-	memcpy(rtc_config.bssid, info.wifi_sta_connected.bssid, sizeof(rtc_config.bssid));
-        break;
-    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED: // 5
-        // Reason codes can be found here: https://github.com/espressif/esp-idf/blob/5454d37d496a8c58542eb450467471404c606501/components/esp_wifi/include/esp_wifi_types_generic.h#L79-L141
-        printf("[%ld] WiFi STA disconnected: %d\n", millis(), info.wifi_sta_disconnected.reason);
-        goToSleep = true;
-        break;
-    case ARDUINO_EVENT_WIFI_STA_GOT_IP: // 7
-        printf("[%ld] WiFi STA got ip: %s/%s gw %s rssi=%d\n", millis(),
-		 WiFi.localIP().toString().c_str(),
-		 WiFi.subnetMask().toString().c_str(),
-		 WiFi.gatewayIP().toString().c_str(),
-		 WiFi.RSSI());
-	rtc_config.ip = WiFi.localIP();
-	rtc_config.gateway = WiFi.gatewayIP();
-	rtc_config.netmask = WiFi.subnetMask();
-	rtc_config.primaryDNS = WiFi.dnsIP(0);
-	rtc_config.secondaryDNS = WiFi.dnsIP(1);
-	if (!rtc_config.dhcp_lease) {
-		rtc_config.dhcp_lease = netif_get_dhcp_lease(info.got_ip.esp_netif);
-		rtc_config.dhcp_renew_remain = rtc_config.dhcp_lease / 2;
+static char mydata[512];
+static uint8_t imgbuf[8192];
+static int imgbufptr = 0;
+static int imgbuflen = sizeof(imgbuf);
+static int pixbufofs = 0;
+
+static void tcp_error(void *arg, int8_t err) {
+	ets_printf("[%ld] +E: %p %d\n", millis(), arg, err);
+}
+
+static int8_t tcp_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *pb, int8_t err) {
+	ets_printf("[%ld] +R: %p %p %d\n", millis(), arg, pcb, err);
+	if (pb != NULL) {
+		ets_printf("rx %d bytes\n", pb->tot_len);
+		memcpy(&imgbuf[imgbufptr], pb->payload, pb->tot_len);
+		if (imgbufptr == 0 && imgbuf[0] == 'B' && imgbuf[1] == 'M') {
+			imgbuflen = imgbuf[2]
+				| (imgbuf[3] << 8)
+				| (imgbuf[4] << 16)
+				| (imgbuf[5] << 24);
+			pixbufofs = imgbuf[10]
+				| (imgbuf[11] << 8)
+				| (imgbuf[12] << 16)
+				| (imgbuf[13] << 24);
+			ets_printf("bmp size: %d (pixbuf @%d)\n", imgbuflen, pixbufofs);
+		}
+		imgbufptr += pb->tot_len;
+		tcp_recved(pcb, pb->tot_len);
+		pbuf_free(pb);
+		if (imgbufptr >= imgbuflen) {
+			pixbufReady = true;
+			tcp_close(pcb);
+		}
+	} else {
+		ets_printf("(connection closed)\n");
+		tcp_close(pcb);
+	}
+	return ERR_OK;
+}
+
+static int8_t tcp_sent(void *arg, struct tcp_pcb *pcb, uint16_t len) {
+	ets_printf("[%ld] +S: %p %p %d\n", millis(), arg, pcb, len);
+	return ERR_OK;
+}
+
+static int8_t tcp_poll(void *arg, struct tcp_pcb *pcb) {
+	ets_printf("[%ld] +P: %p %p\n", millis(), arg, pcb);
+	return ERR_OK;
+}
+
+static int8_t tcp_connected(void *arg, struct tcp_pcb *pcb, int8_t err) {
+	ets_printf("[%ld] +C: %p %p %d\n", millis(), arg, pcb, err);
+	if (err != ERR_OK) {
+		ets_printf("connection error: %d\n", err);
+		return ERR_OK;
 	}
 
-        mqttClient.connect();
-        break;
-    default:
-        printf("[%ld] Unhandled event: %d (%s)\n", millis(), event, WiFi.eventName(event));
-        break;
-    }
+	err = tcp_write(pcb, mydata, strlen(mydata), TCP_WRITE_FLAG_COPY);
+	if (err != ERR_OK) {
+		ets_printf("tcp_write: %d\n", err);
+	}
+	err = tcp_output(pcb);
+	if (err != ERR_OK) {
+		ets_printf("tcp_output: %d\n", err);
+	}
+	return ERR_OK;
+}
+
+static void tcp_do_connect(void *ctx)
+{
+	ip_addr_t addr;
+	addr.type = IPADDR_TYPE_V4;
+	addr.u_addr.ip4.addr = IPAddress(192, 168, 8, 240);
+	constexpr int port = 4341;
+
+	tcp_pcb* pcb = tcp_new_ip_type(IPADDR_TYPE_V4);
+	if (!pcb) {
+		ets_printf("pcb == NULL\n");
+		return;
+	}
+
+	tcp_arg(pcb, ctx);
+	tcp_nagle_disable(pcb);
+	tcp_err(pcb, &tcp_error);
+	tcp_recv(pcb, &tcp_recv);
+	tcp_sent(pcb, &tcp_sent);
+	tcp_poll(pcb, &tcp_poll, 16); /* Every 8s or so */
+	err_t err = tcp_connect(pcb, &addr, port, tcp_connected);
+	if (err != ERR_OK) {
+		ets_printf("tcp_connect: %d\n");
+	}
+}
+
+static void tcp_connect()
+{
+	String rssi(WiFi.RSSI());
+	String batt(intBatV);
+	snprintf(mydata, sizeof(mydata), "rssi=%s\nvbat=%s\n", rssi.c_str(), batt.c_str());
+
+	tcpip_try_callback(tcp_do_connect, nullptr);
+}
+
+static void wifi_event_cb(arduino_event_id_t event, arduino_event_info_t info)
+{
+	uint8_t *bssid;
+	switch (event) {
+	case ARDUINO_EVENT_WIFI_STA_CONNECTED: // 4
+		bssid = info.wifi_sta_connected.bssid;
+		printf("[%ld] WiFi STA connected (ch=%d bss=%02x%02x%02x%02x%02x%02x)\n",
+		millis(), info.wifi_sta_connected.channel, bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5]);
+		rtc_config.channel = info.wifi_sta_connected.channel;
+		memcpy(rtc_config.bssid, info.wifi_sta_connected.bssid, sizeof(rtc_config.bssid));
+		break;
+	case ARDUINO_EVENT_WIFI_STA_DISCONNECTED: // 5
+		// Reason codes can be found here: https://github.com/espressif/esp-idf/blob/5454d37d496a8c58542eb450467471404c606501/components/esp_wifi/include/esp_wifi_types_generic.h#L79-L141
+		printf("[%ld] WiFi STA disconnected: %d\n", millis(), info.wifi_sta_disconnected.reason);
+		goToSleep = true;
+		break;
+	case ARDUINO_EVENT_WIFI_STA_GOT_IP: // 7
+		printf("[%ld] WiFi STA got ip: %s/%s gw %s rssi=%d\n", millis(),
+			WiFi.localIP().toString().c_str(),
+			WiFi.subnetMask().toString().c_str(),
+			WiFi.gatewayIP().toString().c_str(),
+			WiFi.RSSI());
+		rtc_config.ip = WiFi.localIP();
+		rtc_config.gateway = WiFi.gatewayIP();
+		rtc_config.netmask = WiFi.subnetMask();
+		rtc_config.primaryDNS = WiFi.dnsIP(0);
+		rtc_config.secondaryDNS = WiFi.dnsIP(1);
+		if (!rtc_config.dhcp_lease) {
+			rtc_config.dhcp_lease = netif_get_dhcp_lease(info.got_ip.esp_netif);
+			rtc_config.dhcp_renew_remain = rtc_config.dhcp_lease / 2;
+		}
+
+		tcp_connect();
+		break;
+	default:
+		printf("[%ld] Unhandled event: %d (%s)\n", millis(), event, WiFi.eventName(event));
+		break;
+	}
 }
 
 static void wifi_init()
@@ -179,22 +285,6 @@ static void wifi_init()
 	WiFi.setTxPower(WIFI_POWER_11dBm);
 }
 
-constexpr char kMqttBatteryCurrent[] = "solar/battery/current";
-constexpr char kMqttBatterySoC[] = "solar/battery/stateOfCharge";
-constexpr char kMqttInverterPower[] = "solar/ac/power";
-
-void onMqttConnect(bool sessionPresent)
-{
-	printf("[%ld] Connected to MQTT (%d)\n", millis(), sessionPresent);
-	mqttClient.subscribe(kMqttBatteryCurrent, 0);
-	mqttClient.subscribe(kMqttBatterySoC, 0);
-	mqttClient.subscribe(kMqttInverterPower, 0);
-	String rssi(WiFi.RSSI());
-	String batt(intBatV);
-	mqttClient.publish("epaper/sensor/wifi/rssi", 0, true, rssi.c_str());
-	mqttClient.publish("epaper/sensor/battery_voltage/state", 0, true, batt.c_str());
-}
-
 static void dprintf(int x, int y, const GFXfont *font, const char *fmt, ...)
 {
 	char buf[40];
@@ -205,34 +295,6 @@ static void dprintf(int x, int y, const GFXfont *font, const char *fmt, ...)
 	vsnprintf(buf, sizeof(buf), fmt, ap);
 	va_end(ap);
 	display.print(buf);
-}
-
-void onMqttMessage(const espMqttClientTypes::MessageProperties& properties, const char* topic, const uint8_t* payload, size_t len, size_t index, size_t total) {
-	String data(payload, len);
-	printf("[%ld] received topic %s: %s\n", millis(), topic, data.c_str());
-	if (strcmp(topic, kMqttBatteryCurrent) == 0) {
-		batteryCurrent = data.toFloat();
-	} else if (strcmp(topic, kMqttBatterySoC) == 0) {
-		batterySoC = data.toFloat();
-	} else if (strcmp(topic, kMqttInverterPower) == 0) {
-		inverterPower = data.toFloat();
-	}
-	if (isnan(batteryCurrent) || isnan(batterySoC) || isnan(inverterPower)) {
-		return;
-	}
-
-	dataReady = true;
-	WiFi.disconnect(true);
-	WiFi.mode(WIFI_OFF);
-}
-
-static void mqtt_init()
-{
-	mqtt_host.trim();
-
-	mqttClient.onConnect(onMqttConnect);
-	mqttClient.onMessage(onMqttMessage);
-	mqttClient.setServer(mqtt_host.c_str(), 1883);
 }
 
 static float batVolt()
@@ -246,6 +308,66 @@ static float batVolt()
 	return 2.0 * adc.readVoltage();
 }
 
+esp_adc_cal_characteristics_t characteristics;
+
+// The first 512 bytes of RTC_SLOW_MEM are reserved for ULP use, see platformio sdkconfig.h
+// Use the first half to store ADC values.
+constexpr size_t ulp_data_addr = 4;
+constexpr size_t ulp_data_size = CONFIG_ESP32_ULP_COPROC_RESERVE_MEM / 4 / 2;
+constexpr size_t ulp_load_addr = ulp_data_addr + ulp_data_size;
+
+static void dump_adc_data()
+{
+	for (int i = 0; i < 8; i++) {
+		int raw = RTC_SLOW_MEM[ulp_data_addr + i] & 0xffff;
+		int mV = esp_adc_cal_raw_to_voltage(raw, &characteristics);
+		printf("ulp_adc_data[%d] = %d (%d/%x)\n", i, mV, raw, raw);
+	}
+	for (int i = ulp_data_size - 8; i < ulp_data_size; i++) {
+		int raw = RTC_SLOW_MEM[ulp_data_addr + i] & 0xffff;
+		int mV = esp_adc_cal_raw_to_voltage(raw, &characteristics);
+		printf("ulp_adc_data[%d] = %d (%d/%x)\n", i, mV, raw, raw);
+	}
+}
+
+static void ulp_init()
+{
+	const ulp_insn_t program[] = {
+		I_MOVI(R0, 0),          // R0 <- 0
+		M_LABEL(1),             // label_1
+		I_ADC(R3, 0, 7),        // Read ADC from ADC1 input 7
+		I_MOVI(R2, ulp_data_addr),  // R1 <- data_addr
+		I_ADDR(R2, R2, R0),     // R2 <- R2 + R0
+		I_ST(R3, R2, 0),        // R3 -> RTC_SLOW_MEM[R2 + 0]
+		I_ADDI(R0, R0, 1),      // R0 <- R0 + 1
+		M_BL(1, ulp_data_size),     // if (R0 < data_size) goto label_1
+		I_HALT(),
+	};
+	size_t size = sizeof(program)/sizeof(ulp_insn_t);
+
+	esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_12, (adc_bits_width_t)ADC_WIDTH_BIT_DEFAULT, 1100/*VREF*/, &characteristics);
+
+	esp_err_t err = ulp_process_macros_and_load(ulp_load_addr, program, &size);
+	printf("ulp program size=%d err=%04x\n", (int)size, err);
+	printf("load_addr=%04x\n", (int)ulp_load_addr);
+	printf("data_addr=%04x\n", (int)ulp_data_addr);
+	for (int i = 0; i < size; i++) {
+		printf("program[%d]=%08x\n", i, RTC_SLOW_MEM[ulp_load_addr + i]);
+	}
+	dump_adc_data();
+
+	rtc_gpio_init(GPIO_NUM_35);
+	rtc_gpio_set_direction(GPIO_NUM_35, RTC_GPIO_MODE_INPUT_ONLY);
+	rtc_gpio_set_direction_in_sleep(GPIO_NUM_35, RTC_GPIO_MODE_INPUT_ONLY);
+	rtc_gpio_hold_en(GPIO_NUM_35);
+	adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_12);
+	adc1_config_width(ADC_WIDTH_BIT_12);
+	adc1_ulp_enable();
+	// wake-up ULP from halt once per second
+	ulp_set_wakeup_period(0, 1000 * 1000);
+	ulp_run(ulp_load_addr);
+}
+
 void setup()
 {
 	bool full_init = survives_sleep == 0;
@@ -253,8 +375,8 @@ void setup()
 	intBatV = batVolt();
 
 	printf("Hello world (wake=%d) bat=%.2fV t=%.1f!\n", survives_sleep++, intBatV, temp);
+	ulp_init();
 	fs_init(full_init);
-	mqtt_init();
 	wifi_init();
 	hspi.begin(/*SCK*/18, /*MISO*/-1, /*MOSI*/23, /*SS*/-1);
 	display.epd2.selectSPI(hspi, SPISettings(10000000, MSBFIRST, SPI_MODE0));
@@ -278,23 +400,18 @@ void setup()
 		display.refresh(true);
 	}
 	printf("[%ld] setup done\n", millis());
+	dump_adc_data();
 }
 
 void loop()
 {
-	if (dataReady) {
-		float p = (1-(4.1-intBatV)/(4.1-3.3))*100;
-		p = p < 100 ? p : 100;
-
+	if (pixbufReady) {
 		printf("[%ld] update display\n", millis());
+		delay(1);
+		WiFi.disconnect(true);
+		WiFi.mode(WIFI_OFF);
 		display.firstPage();
-		display.fillScreen(GxEPD_WHITE);
-		display.setTextColor(GxEPD_BLACK);
-
-		dprintf(0, 4+28, &FreeSans18pt7b, "I: %.0fW", inverterPower);
-		dprintf(0, 4+56, &FreeSans18pt7b, "B: %.0f%%", batterySoC);
-		dprintf(0, 4+84, &FreeSans18pt7b, "%.2fV %.1f%%", intBatV, p);
-
+		display.drawBitmap(0, 0, &imgbuf[pixbufofs], 250, 122, GxEPD_WHITE, GxEPD_BLACK);
 		display.display(/*partial*/ true);
 		goToSleep = true;
 	}
@@ -302,19 +419,36 @@ void loop()
 		long sleepTime = 30;
 		rtc_config.dhcp_renew_remain -= sleepTime;
 
-		long sleepMillis = sleepTime * 1000 - millis();
-		printf("[%ld] going to sleep for %ldms\n", millis(), sleepMillis);
 		display.hibernate();
-		esp_sleep_enable_timer_wakeup(sleepMillis * 1000);
+
 		// Make sure epaper CS# is kept high during sleep.
 		pinMode(16, INPUT_PULLUP);
 		printf("[%ld] esp_deep_sleep_start()\n", millis());
+#if 0
 		gpio_deep_sleep_hold_en();
+		gpio_hold_dis((gpio_num_t)batPin);
+#endif
 #if 0
 		// Make sure SDIO power domain (SPI flash) is disabled during sleep.
 		// Doesn't do anything here, as it's also directly connected to +3V3...
 		esp_sleep_pd_config(ESP_PD_DOMAIN_VDDSDIO, ESP_PD_OPTION_OFF)
 #endif
+
+		long sleepMillis = sleepTime * 1000 - millis();
+		printf("[%ld] going to sleep for %ldms\n", millis(), sleepMillis);
+		// esp_deep_sleep_disable_rom_logging();
+		// Need to re-init ADC after wifi stop? https://github.com/espressif/esp-idf/issues/10471
+		adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_12);
+		adc1_config_width(ADC_WIDTH_BIT_12);
+		adc1_ulp_enable();
+		ulp_run(ulp_load_addr);
+		esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+		esp_sleep_enable_ulp_wakeup();
+		esp_sleep_enable_timer_wakeup(sleepMillis * 1000);
+		rtc_gpio_init(GPIO_NUM_35);
+		rtc_gpio_set_direction(GPIO_NUM_35, RTC_GPIO_MODE_INPUT_ONLY);
+		rtc_gpio_set_direction_in_sleep(GPIO_NUM_35, RTC_GPIO_MODE_INPUT_ONLY);
+		rtc_gpio_hold_en(GPIO_NUM_35);
 		esp_deep_sleep_start();
 
 		// Deep sleep power draw measured ~394uA after adding a pull-up
@@ -322,3 +456,17 @@ void loop()
 	}
 	delay(1);
 }
+
+extern "C" {
+
+static RTC_RODATA_ATTR const char kWakeStub[] = "Wake stub!\n";
+
+// Note: RTC IRAM/FAST memory is only accessible to the "PRO" core
+void RTC_IRAM_ATTR esp_wake_deep_sleep(void) {
+	ets_printf(kWakeStub);
+	esp_default_wake_deep_sleep();
+	// Add additional functionality here
+}
+
+}
+
